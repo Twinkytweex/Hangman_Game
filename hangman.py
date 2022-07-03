5c4bbb35c9690087a9f22632d4e61e55deeebdd3
Twinkytweex <95413969+Twinkytweex@users.noreply.github.com>
2022-07-03 22:07:53 +0400

    Update hangman.py
    Game is ready to play...

--- hangman.py
+++ hangman.py
@@ -1,6 +1,9 @@
-#Step 3
+#Hangman Game
+
+#importing random module
 
 import random
+from re import A
 word_list = ["apple", "banana", "camel"]
 chosen_word = random.choice(word_list)
 word_length = len(chosen_word)
@@ -14,11 +17,26 @@ for _ in range(word_length):
     display += "_"
 print(display)
 
+#creating Health system
+live = 6
+
 #Check guessed letter
-while "_" in display:
-  guess = input("Guess a letter: ").lower()
-  for position in range(word_length):
-    letter = chosen_word[position]
-    if letter == guess:
-      display[position] = letter
-  print(display) 
+while live != 0:
+      #winning condition
+      if "_" not in display:
+          print("Congrats you won the game!")
+      guess = input("Guess a letter: ").lower()
+      #checking if its correct
+      for position in range(word_length):            
+            letter = chosen_word[position]
+            if guess in letter:                  
+                  display[position] = letter
+                  print(display)
+      #cheking if its incorrect and -1 if not
+      if guess not in chosen_word:
+            print("you are wrong")
+            live -= 1
+            print(f"you're life left: {live}") 
+# loseing the game
+if live == 0:
+      print("you lose the game!!!")
